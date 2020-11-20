4ff363fcde1a1a4ba80fea64ccd8363aff14b88b
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-20 11:46:17 +0100

    Correction formulaires

--- forms/sign_in.php
+++ forms/sign_in.php
@@ -7,19 +7,7 @@ include 'include/header.php';
 <html>
 <body>
 
-  <form action="../back/inscription_backend.php" method="post">
-    <div class="form-group row">
-      <div class="">
-        <input type="text" class="form-control" name="nom" placeholder="Nom"  maxlength="80" required>
-    </div>
-  </div>
-
-  <div class="form-group row">
-    <div class="">
-      <input type="text" class="form-control" name="prenom" placeholder="Prénom" maxlength="80" required>
-    </div>
-  </div>
-
+  <form action="../back/connexion_backend.php" method="post">
   <div class="form-group row">
     <div class="">
       <input type="email" class="form-control" name="mail" placeholder="Email" required>
