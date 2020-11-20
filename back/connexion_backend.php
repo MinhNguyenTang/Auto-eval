4090b6d95ba0ca5c5e6895a428737d7a30de2d75
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-20 11:48:28 +0100

    Première version traitement

---
+++ back/connexion_backend.php
@@ -0,0 +1,17 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/entity/user.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/manager/manager.php');
+
+$user = new user($_POST);
+$manager = new manager();
+$result = $manager->connexion($user);
+if($result)
+{
+  session_start();
+  $_SESSION['user'] = serialize($result);
+  header('Location: ../index.php');
+}
+else {
+  header('Location: ../forms/sign_in.php');
+}
+ ?>
--- back/inscription_backend.php
+++ back/inscription_backend.php
@@ -1,7 +1,21 @@
 <?php
 
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/entity/user.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/manager/manager.php');
+
 $new_user = new user($_POST);
-$new_formateurs = 
+$new_formateurs = new formateurs($_POST);
 $manager = new manager();
 $manager->insert_user($new_user);
+
+$new_user->setRole_user('user');
+$result = $manager->insert_user($new_user);
+if($result==1)
+{
+  echo $result;
+  header('Location: ../forms/sign_in.php');
+} else {
+  echo $result;
+  header('Location: ../forms/sign_up.php');
+}
  ?>
