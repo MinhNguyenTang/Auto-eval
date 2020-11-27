a35c2242ed6766d04576adcda9e4cce7bebb0f0e
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-11-27 14:18:01 +0100

    Modif

--- include/header.php
+++ include/header.php
@@ -6,7 +6,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/manager/manager.php');
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>Learn &mdash; Home</title>
+<title>Learn</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
 <meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
