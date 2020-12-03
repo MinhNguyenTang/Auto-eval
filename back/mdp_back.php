d11ded0cb01556d6c8fa13ea4f5b71617b848cf0
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-12-03 14:53:48 +0100

    Réinitialisation du mot de passe back

--- back/mdp_back.php
+++ back/mdp_back.php
@@ -5,5 +5,7 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/entity/user.php');
 
 $user = new user($_POST);
 $manager = new manager();
+$user->setMail($_POST['mail']);
+$user->setMdp($_POST['mdp']);
 $manager->new_mdp($user);
  ?>
