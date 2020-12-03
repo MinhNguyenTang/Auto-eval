e6527aac440645f789aeb20fffa651919562dbde
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-12-03 04:52:20 +0100

    Espace admin

---
+++ web/admin.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<?php
+include "../include/header.php";
+ ?>
+
+<body>
+
+<div class="row-form group">
+  <a href="../forms/nouveau_formateur.php">Ajouter un formateur</a>
+  <a href=../forms/nouveau_admin.php>Ajouter un administrateur</a>
+</div>
+
+  <?php
+  include "../include/footer.php";
+   ?>
+</body>
+</html>
