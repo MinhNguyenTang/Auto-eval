76223e7fa89ba98b715b12189a6b27888117f80f
MinhNguyenTang <minhnguyyen7@gmail.com>
2020-12-10 10:23:06 +0100

    Panel administrateur

--- web/admin.php
+++ web/admin.php
@@ -3,14 +3,58 @@
 
 <?php
 include "../include/header.php";
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/entity/user.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/entity/spe.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/manager/manager.php');
  ?>
 
 <body>
 
-<div class="row-form group">
-  <a href="../forms/nouveau_formateur.php">Ajouter un formateur</a>
-  <a href=../forms/nouveau_admin.php>Ajouter un administrateur</a>
-</div>
+  <div class="container">
+    <div class=" form-group">
+      <div class="title">
+        <h3>Tableau de bord</h3>
+      </div>
+    </div>
+    <div class=" form-group">
+      <div class="title">
+        <h4>Formateurs<h4>
+      </div>
+      <div class="table">
+        <table class="black white-text">
+          <thead>
+            <tr>
+              <th scope="col">Nom</th>
+              <th scope="col">Prénom</th>
+              <th scope="col">Spécialités</th>
+              <th scope="col">Rôles</th>
+            </tr>
+          </thead>
+          <?php
+          $manager = new manager();
+          if(unserialize($_SESSION['user'])->getRole_user()=="formateur") {
+          $formateurs = $manager->afficher_formateurs();
+           ?>
+          <tbody>
+            <tr>
+              <?php foreach($formateurs as $key => $value) { ?>
+              <td><?php echo $value['nom']; ?></td>
+              <td><?php echo $value['prenom']; ?></td>
+              <td><?php echo $value['nom_spe']; ?></td>
+              <td><?php echo $value['role_user']; ?></td>
+              <?php } ?>
+            </tr>
+          </tbody>
+        <?php } ?>
+        </table>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group row">
+    <a href="../forms/nouveau_formateur.php" class="btn btn-info">Ajouter un formateur</a>
+    <a href="../forms/nouveau_admin.php" class="btn btn-info">Ajouter un administrateur</a>
+  </div>
 
   <?php
   include "../include/footer.php";
