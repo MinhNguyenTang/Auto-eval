5d70af13e74909f7ffa1a492929e3ff5a7f27ea9
MinhNguyenTang <56515817+MinhNguyenTang@users.noreply.github.com>
2021-03-03 10:36:23 +0100

    Traitement

--- back/nouveau_mdp_back.php
+++ back/nouveau_mdp_back.php
@@ -7,5 +7,5 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/Auto-eval/back/manager/manager.php');
 
 $new_pwd = new user($_POST);
 $manager = new manager();
-$manager->nouveau_mdp($new_pwd);
+$manager->modifier_mdp($new_pwd);
  ?>
